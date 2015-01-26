47e690d7ff7c7450a7ea027d5e2d1c603ed0a02a
bandless55 <stevanproductions@gmail.com>
2015-01-26 01:20:00 -0600

    yes

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-//Main entry point for the server
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res){
+    res.send("Yeah Whatever Man");
+});
+
+var server = app.listen(3000, function (){
+    var host = server.address().address;
+    var port = server.address().port;
+    
+    console.log("Bootstrap test listening at http://%s:%s", host, port);
+});
