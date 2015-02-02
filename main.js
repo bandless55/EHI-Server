318976a1f30641a110fa83585848ecf5ac9c2d1a
Sebastian C <sebscoolmail@gmail.com>
2015-02-02 16:35:08 -0600

    basic features of the database

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //Main entry point for the server
 
 //all the main component imports
-var webServer = require('./web-server/Server.js');
-var Preferences = require('./Preferences/PreferencesManager');
+var webServer = require('./web-server/Server');
+var Preferences = require('./preferences/PreferencesManager');
+var Database = require('./database/influxdb');
 
 //initialize in order
 //So we load the config FIRST, then when it is done
@@ -10,6 +11,7 @@ var Preferences = require('./Preferences/PreferencesManager');
 Preferences.load().then( function(){
 	
 	console.log("Prefs loaded");
+	Database.init();
 	//load some other things here... if they do stuff thats async,
 	//and you need to wait for it to finish
 	//have them return a Promise and then use another then
